be521a1363867daa5ae8e5569f64b4fc983c16f2
HizbullahBhatti <hizbullah.kcsf18@iba-suk.edu.pk>
2023-01-07 18:11:09 +0500

    XMLHttpRequest

--- index.js
+++ index.js
@@ -339,7 +339,19 @@ sum(1)(2)(3)
 //             },5000)
 //         },5000)
 //     },5000)
-// },5000)
+// },5000)      
+
+//XML HTTP Request
+
+const request = new XMLHttpRequest();
+
+request.open("GET","https://jsonplaceholder.typicode.com/todos")
+request.send();
+
+request.addEventListener("load",function(){
+    console.log(this.responseText)
+})
+
 
 
 
